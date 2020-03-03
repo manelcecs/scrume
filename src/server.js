3ebb3dbcf83d7f0ea9d0c16e22f2aef5e8b7a139
Manuel C Pérez <manuelc.p.g@gmail.com>
2020-03-03 17:27:34 +0100

    try to deploy

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/flujoCompleto'));
+app.use(express.static(__dirname + '../dist/flujoCompleto'));
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname+'/dist/flujoCompleto/index.html'));
+    res.sendFile(path.join(__dirname+'../dist/flujoCompleto/index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
