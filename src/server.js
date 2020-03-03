9adb058f126f8f7d909b71ebe1b7d2defc9bedb9
Manuel C Pérez <manuelc.p.g@gmail.com>
2020-03-03 18:05:32 +0100

    312try

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/flujo-completo'));
+app.use(express.static(__dirname + '/'));
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname+'/flujo-completo/index.html'));
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
