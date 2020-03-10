00c287a8564d8c41e695a895d9aa068cf790c3c2
Juanma <lorenzoperez.juanmaria@gmail.com>
2020-03-10 19:54:12 +0100

    feat (app): Cambio de log y color de la navbar

---
+++ src/app/dominio/sprint.domain.ts
@@ -0,0 +1,10 @@
+export interface sprint {
+    id: Number;
+    starDate: Date;
+    endDate: Date;
+    totalTasks: Number;
+    completedTasks: Number;
+    totalHP: Number;
+    completedHP: Number;
+
+}
